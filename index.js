144adcfdb67568a4e41a5016a819f258714b6dfe
kibriarobin97 <kibriarobin97@gmail.com>
2024-05-10 15:45:41 +0600

    added service by id

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await serviceCollection.findOne(query)
+        res.send(result)
+      })
+
 
 
     // Send a ping to confirm a successful connection
